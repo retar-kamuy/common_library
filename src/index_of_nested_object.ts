8d0334042ba48f46099bd62fbfe14c1311796f80
Hoshi Takumi <hoshi.takumi.jp@gmail.com>
2022-04-24 11:07:44 +0900

    Add

---
+++ src/index_of_nested_object.ts
@@ -0,0 +1,43 @@
+/**
+ * Target a given value in a nested JSON object
+ *
+ * This module modified return value from value of object to object.
+ *
+ * Reference URL {@link https://www.w3resource.com/javascript-exercises/fundamental/javascript-fundamental-exercise-6.php}
+ *
+ * Attributes:
+ * @param objs (object[]): object matching specified target.
+*/
+export default class IndexOfNestedObject {
+	private objs: any;
+	private max_count: number | undefined;
+
+	constructor(max_count?: number) {
+		this.objs = [];
+		this.max_count = max_count;
+	}
+
+	get_result(): any {
+		return this.objs;
+	}
+
+	run(obj: any, target: string): any {
+		if(this.max_count === 0)
+			return;
+
+		if(Object.values(obj).indexOf(target) > -1) {
+			this.objs.push(obj);
+			if (this.max_count !== undefined)
+				this.max_count -= 1;
+		}
+		else {
+			Object.values(obj).reduce((acc: any, val: any) => {
+				if (acc !== undefined)
+					return acc;
+				if (typeof val === 'object')
+					if(val !== null)
+						return this.run(val, target);
+			}, undefined);
+		}
+	}
+}
